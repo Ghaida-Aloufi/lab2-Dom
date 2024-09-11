5fcd4c5b0df49ad8e9714b54f1d99a0ecb83cd67
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-11 14:22:04 +0300

    add lab2-Dom

---
+++ main.js
@@ -0,0 +1,16 @@
+let x = document.getElementById("seq")
+
+setInterval(() => {
+  x.style.left = Math.floor(Math.random() * 800) + "px";
+  x.style.right= Math.floor(Math.random() * 800) + "px";
+  x.style.bottom= Math.floor(Math.random() * 800) + "px";
+  x.style.top = Math.floor(Math.random() * 800) + "px";
+
+  x.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  x.style.height = Math.floor(Math.random() * 800) + "px";
+  x.style.width = Math.floor(Math.random() * 800) + "px";
+
+
+
+
+}, 1000);
